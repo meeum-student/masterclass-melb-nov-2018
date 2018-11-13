0a9a6908b328c2c0f9271bd7db2e69b4cb469050
Sam <sam@meeum.com>
2018-11-13 16:06:41 +1100

    hero images on home and about

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -18,3 +18,18 @@ $('.nav-prompt').click(function () {
 
 });
 
+
+
+// nav active class
+
+function activeMenu() {
+
+    var url = window.location.href;
+
+    $('.nav-inner a').filter(function() {
+
+        return this.href == url;
+    }).addClass('active');
+}
+
+activeMenu();
